a6a9f0504e1d8086e97b3e4ceadebc7da4097791
Simina Popescu <simina.popescu@endava.com>
2019-01-14 11:23:33 +0200

    Initial Commit

---
+++ RealmTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTutorial
+//
+//  Created by Simina Popescu on 14/01/2019.
+//  Copyright © 2019 SiminaTutorials. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
