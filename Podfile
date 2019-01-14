d26e7b1c49f28040fa1c564c097a5fd41565d22c
Simina Popescu <simina.popescu@endava.com>
2019-01-14 11:32:56 +0200

    added and installed the pod for realmSwift

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+platform :ios, '11.0'
+
+target 'RealmTutorial' do
+  use_frameworks!
+	pod 'RealmSwift'
+end
---
+++ Pods/Target Support Files/Pods-RealmTutorial/Pods-RealmTutorial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmTutorial : NSObject
+@end
+@implementation PodsDummy_Pods_RealmTutorial
+@end
